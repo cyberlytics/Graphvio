834d1e27b22ba367f5b91fbd07430999293f32b7
Stephan Prettner <s.prettner@oth-aw.de>
2021-12-06 13:12:14 +0100

    Metadaten der abgefragten Filme hinzugefügt Unterstützung der sparql_statements.js für andere Provider hinzugefügt
    Former-commit-id: 971b3679b48e3217d92e3d7dbe306a99c6be1fa3

--- src/images/frontend/backend/routes/sparql_statements.js
+++ src/images/frontend/backend/routes/sparql_statements.js
@@ -1,26 +1,52 @@
-var db_port = process.env.DATABASE_PORT
+var DB_PORT = process.env.DATABASE_PORT
 
-function SELECT_ALL_TITLES() {
+function SELECT_ALL_TITLES(provider="netflix", limit=10) {
     sparql =         
-    `PREFIX netflix: <http://localhost:${db_port}/schemas/netflix/> ` +
-    'SELECT * ' +
-    `FROM <http://localhost:${db_port}/netflix#> ` +
-    'WHERE {?id netflix:title ?title ' +
-    `OPTIONAL {?id netflix:director ?director} }` +
-    'LIMIT 10'
+    getPrefix(provider) +
+    `SELECT * ` +
+    getGraph(provider) +
+    `WHERE {?id netflix:title ?title ` +
+    getMetadata() +
+    `} ` +
+    `LIMIT ${limit}`
     return sparql
 }
 
-function SEARCH_MOVIE(movie) {
+function SEARCH_MOVIE(movie, provider="netflix", limit=10) {
     sparql =         
-    `PREFIX netflix: <http://localhost:${db_port}/schemas/netflix/> ` +
+    getPrefix(provider) +
     'SELECT * ' +
-    `FROM <http://localhost:${db_port}/netflix#> ` +
-    'WHERE {?id netflix:title ?title ' +
+    getGraph(provider) +
+    `WHERE {?id movie:title ?title ` +
     `FILTER regex(str(lcase(?title)), "${movie.toLowerCase()}"). ` +
-    `OPTIONAL {?id netflix:director ?director} }` +
-    'LIMIT 10'
+    getMetadata() +
+    `} ` +
+    `LIMIT ${limit}`
     return sparql
+}
+
+function getPrefix(provider, prefix = 'movie') {
+    return `PREFIX ${prefix}: <http://localhost:${DB_PORT}/schemas/${provider}/> `
+}
+
+function getGraph(provider) {
+    return `FROM <http://localhost:${DB_PORT}/${provider}#> `
+}
+
+function getMetadata(prefix = 'movie') {
+    metadata = 
+    `OPTIONAL {?id ${prefix}:cast ?cast} ` +
+    `OPTIONAL {?id ${prefix}:country ?country} ` +
+    `OPTIONAL {?id ${prefix}:date_added ?date_added} ` +
+    `OPTIONAL {?id ${prefix}:description ?description} ` +
+    `OPTIONAL {?id ${prefix}:director ?director} ` +
+    `OPTIONAL {?id ${prefix}:duration ?duration} ` +
+    `OPTIONAL {?id ${prefix}:release_year ?release_year} ` +
+    `OPTIONAL {?id ${prefix}:type ?type} ` +
+    `OPTIONAL {?id ${prefix}:rating ?rating} ` +
+    `OPTIONAL {?id ${prefix}:genre ?genre} `
+
+    return metadata
 }
 
 module.exports = {
