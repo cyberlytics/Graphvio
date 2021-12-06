ad06338d87ceb77562abae964208d534fc645004
Helge Kohl <cb49@oth-aw.de>
2021-12-06 14:03:46 +0100

    IMDB-Rating Abfrage hinzugefügt
    Former-commit-id: d33528d3d27f51503b7619e7cbc45e917955296b

--- src/images/frontend/backend/index.js
+++ src/images/frontend/backend/index.js
@@ -2,6 +2,8 @@ const express = require('express');
 const cors = require('cors');
 
 const dbRouter = require('./routes/db');
+const imdbRouter = require('./routes/imdb');
+console.log(imdbRouter);
 
 const app = express();
 const port = process.env.PORT || 5000;
@@ -10,6 +12,7 @@ app.use(cors());
 app.use(express.json());
 
 app.use('/db', dbRouter.router);
+app.use('/imdb', imdbRouter.router);
 
 app.listen(port, () => {
     console.log(`Server is running on port: ${port}`);
